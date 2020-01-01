72e6f5d3b14c3be681be7401fe8f3b0491b62bee
Chethan H <chethan.h@genpact.digital>
2020-01-01 14:17:07 +0530

    initial commit

---
+++ testingCi/src/main/java/testingCricleCi/testingCi/App.java
@@ -0,0 +1,13 @@
+package testingCricleCi.testingCi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
